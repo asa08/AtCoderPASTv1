801e9ebb7bbf582d3cda48654d6546997139b7c2
maiko-m <mn.6182.nm@gmail.com>
2023-03-01 00:33:49 +0900

    追加

--- AtCoderPASTv1/main.swift
+++ AtCoderPASTv1/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-// 標準入力
+// MARK: - 標準入力
 func readInt() -> Int {
     return Int(readLine()!)!
 }
@@ -26,142 +26,66 @@ func readThreeInts() -> (a: Int, b: Int, c: Int) {
     return (a: ints[0], b: ints[1], c: ints[2])
 }
 
-qe()
+otoshidama_1()
 
-// 問題
-func qe() {
-    // n 人数
-    // q 回数
-    let (n, q) = readTwoInts()
-    let array = (0..<q).map{ _ in readInts() }
-    var follow: [[Bool]] = []
-    
-    let followInit: [Bool] = (0..<n).map{ _ in false }
-    
-    for index in 0..<n {
-        follow.append(followInit)
+// MARK: - Product
+func product() {
+    let (a, b) = readTwoInts()
+    print((a * b).isMultiple(of: 2) ? "Even" : "Odd")
+}
+
+// MARK: - Placing Marbles
+func placingMarbles() {
+    let input = readLine()!
+    let conut = input.reduce(0) { $0 + Int(String($1))! }
+    print(conut)
+}
+
+// MARK: - Shift Only
+func shiftOnly() {
+    let count = readInt()
+    var ints = readInts()
+    var result = 0
+    while ints.allSatisfy({ $0.isMultiple(of: 2) }) {
+        result += 1
+        ints = ints.map({ $0 / 2 })
     }
-    
-    for action in array {
-        let type = action[0]
-        if type == 1 {
-            let from = action[1]
-            let to = action[2]
-            follow[from][to-1] = true
-        } else if type == 2 {
-            var me = action[1]
-            for row in 0..<follow.count {
-                if follow[row]
+    print(result)
+}
+
+// MARK: - Otoshidama
+func otoshidama_1() {
+    let (N, Y) = readTwoInts()
+
+    for i in 0...N {
+        for j in 0...N {
+            for k in 0...N {
+                if N < i + j + k {
+                    continue
+                }
+                if Y == (i * 10000) + (j * 5000) + (k * 1000) {
+                    print(i, j, k)
+                    return
+                }
             }
-        } else if type == 3 {
-            
         }
     }
-    
-    print(follow)
+
+    print(-1, -1, -1)
 }
 
-func qd() {
-    let n = readInt()
-    let array = (0..<n).map{ _ in readInt() }
-    let set = Set(array)
-    if set.count == n {
-        print("Correct")
-        return
-    }
-    var dic: [Int: Int] = [:]
-    
-    // O(n)
-    for nom in 0..<n {
-        let index = nom + 1
-        dic[index] = 0
-    }
-    
-    // O(n)
-    for nom in 0..<n {
-        let index = nom + 1
-        
-        if (index) == array[nom] {
-            dic[index]! += 1
-        } else {
-            dic[array[nom]]! += 1
+func otoshidama_2() {
+    let (N, Y) = readTwoInts()
+
+    for i in 0...N {
+        for j in 0...N {
+            let k = N - i - j
+            if k >= 0, Y == (i * 10000) + (j * 5000) + (k * 1000) {
+                print(i, j, k)
+                return
+            }
         }
     }
-    
-    var y = 0
-    var x = 0
-    // O(n)
-    for set in dic {
-        if set.value == 2 {
-            y = set.key
-        }
-        if set.value == 0 {
-            x = set.key
-        }
-    }
-    print(y, x)
-}
 
-// botu
-// TLE
-// filter: O(n)
-// for: O(n)
-
-//func qd() {
-//    let n = readInt()
-//    var x = 0
-//    var y = 0
-//    let array = (0..<n).map{ _ in readInt() }
-//    for index in 1...n {
-//        let count = array.filter{ $0 == index }.count
-//        if count == 0 {
-//            x = index
-//        }
-//        if count == 2 {
-//            y = index
-//        }
-//    }
-//    if x == 0 && y == 0 {
-//        print("Correct")
-//        return
-//    }
-//    print(y, x)
-//}
-
-func qc() {
-    let ints = readLine()!.split(separator: " ").map { Int(String($0))! }
-    var array = Array(ints)
-    array.sort { $1 < $0 }
-    print(array[2])
-}
-
-func qb() {
-    let n = readInt()
-    var array: [Int] = []
-    array = (0..<n).map{ _ in readInt() }
-    for index in 0..<array.count {
-        if index == array.count - 1 {
-            return
-        }
-        let diff = array[index+1] - array[index]
-        if diff < 0 {
-            print("down", abs(diff))
-        } else if diff > 0 {
-            print("up", abs(diff))
-        } else {
-            print("stay")
-        }
-    }
-}
-
-func qa() {
-    let read = readLine()!
-    let array = Array(read)
-    let isNum = array.allSatisfy({ Int(String($0)) != nil })
-    if isNum {
-        let result = Int(String(read))! * 2
-        print(result)
-    } else {
-        print("error")
-    }
+    print(-1, -1, -1)
 }
